218e5c0f1f9be9d948de5873cd4e1c4677b30e47
Ulises Alexander AM <30351520+UlisesAlexanderAM@users.noreply.github.com>
2024-04-27 12:49:28 -0600

    feat(get_skill): show the skill in a table

--- src/skillventory_pycli/main.py
+++ src/skillventory_pycli/main.py
@@ -2,7 +2,7 @@ import cyclopts
 import httpx
 import pydantic_settings
 from pydantic import AnyUrl
-from rich import console
+from rich import console, table
 
 
 class Settings(pydantic_settings.BaseSettings):
@@ -14,6 +14,7 @@ console = console.Console()
 
 app = cyclopts.App(help="CLI tool to interact with a skillventory API")
 
+skill_table = table.Table(title="Skills")
 
 @app.command(name="get")
 def get_skill(*, url: AnyUrl = settings.skillventory_url, name: str) -> None:
@@ -27,7 +28,10 @@ def get_skill(*, url: AnyUrl = settings.skillventory_url, name: str) -> None:
         response = httpx.get(f"{url}v1/skills/name/{name}")
         response.raise_for_status()
         skill = response.json()
-        console.print(f"Skill: {skill["skill_name"]}\nLevel of confidence: {skill["level_of_confidence"]}")
+        skill_table.add_column("Skill name", style="blue")
+        skill_table.add_column("Level of confidence")
+        skill_table.add_row(skill["skill_name"],skill["level_of_confidence"])
+        console.print(skill_table)
 
 
 def cli():
